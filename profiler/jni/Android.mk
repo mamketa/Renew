ea27e40042f1ae4ebfb9eaaf2adc99a62b892194
GorexTest <googlingrex@gmail.com>
2026-04-18 15:42:14 +0700

    fix adalah pokoknya 3

--- profiler/jni/Android.mk
+++ profiler/jni/Android.mk
@@ -1,6 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := velfox_profiler
 LOCAL_SRC_FILES := \
     ../main.c \
@@ -17,29 +18,32 @@ LOCAL_SRC_FILES := \
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include
 
 LOCAL_CFLAGS := \
-    -DNDEBUG \
-    -O2 \
-    -std=c23 \
+    -O3 \
+    -std=c17 \
+    -fPIE \
     -fPIC \
+    -fvisibility=hidden \
+    -fdebug-prefix-map=$(LOCAL_PATH)=. \
+    -DNDEBUG \
     -D_GNU_SOURCE \
     -D_FORTIFY_SOURCE=2 \
     -fstack-protector-strong \
-    -funwind-tables \
     -ffunction-sections \
     -fdata-sections \
     -flto \
     -Wall \
     -Wextra \
-    -Wno-unused-parameter \
-    -Wno-unused-variable \
-    -Wno-sign-compare
+    -Wno-unused-parameter
 
 LOCAL_LDFLAGS := \
     -flto \
+    -pie \
     -Wl,--gc-sections \
     -Wl,-z,relro \
     -Wl,-z,now
-    
+
 LOCAL_LDLIBS += -llog
+
+LOCAL_STRIP_MODULE := true
 
 include $(BUILD_EXECUTABLE)
